3699725e38ec73162bf9a1bccf04d5f39d905fba
SerhiyStepanov <SerhiyStepanov@users.noreply.github.com>
2020-11-26 21:25:01 +0200

    task 1

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ export default function App() {
         title={user.name}
         tag={user.tag}
         location={user.location}
+        stats={user.stats}
       />
     </div>
   );
--- src/components/user.js
+++ src/components/user.js
@@ -1,32 +1,44 @@
-export default function Profile(props) {
+import PropTypes from "prop-types";
+
+export default function Profile(
+  { url, title, tag, location, stats },
+  children
+) {
   return (
     <div className="profile">
       <div className="description">
         <img
-          src={props.url}
+          src={url}
           alt="Аватар пользователя"
           className="avatar"
           width="240"
         />
-        <p className="name">{props.title}</p>
-        <p className="tag">{props.tag}</p>
-        <p className="location">{props.location}</p>
+        <p className="name">{title}</p>
+        <p className="tag">{tag}</p>
+        <p className="location">{location}</p>
       </div>
 
       <ul className="stats">
         <li>
           <span className="label">Followers</span>
-          <span className="quantity">1000</span>
+          <span className="quantity">{stats.followers}</span>
         </li>
         <li>
           <span className="label">Views</span>
-          <span className="quantity">2000</span>
+          <span className="quantity">{stats.views}</span>
         </li>
         <li>
           <span className="label">Likes</span>
-          <span className="quantity">3000</span>
+          <span className="quantity">{stats.likes}</span>
         </li>
       </ul>
     </div>
   );
 }
+
+Profile.propTypes = {
+  url: PropTypes.string,
+  title: PropTypes.string,
+  tag: PropTypes.string,
+  location: PropTypes.string,
+};
