cc74492385c508a5a8bc9b55cb659f5de201d58a
SerhiyStepanov <SerhiyStepanov@users.noreply.github.com>
2020-11-27 13:15:21 +0200

    add task 1

--- src/components/user.js
+++ src/components/user.js
@@ -1,36 +1,31 @@
 import PropTypes from "prop-types";
-import styles from "./User.module.css";
+import s from "./User.module.css";
 
 export default function Profile(
   { url, title, tag, location, stats },
   children
 ) {
   return (
-    <div className="profile">
+    <div className={s.profile}>
       <div className="description">
-        <img
-          src={url}
-          alt="Аватар пользователя"
-          className="avatar"
-          width="240"
-        />
-        <p className="name">{title}</p>
-        <p className="tag">{tag}</p>
-        <p className="location">{location}</p>
+        <img src={url} alt="Аватар пользователя" className={s.avatar} />
+        <p className={s.name}>{title}</p>
+        <p className={s.tag}>{tag}</p>
+        <p className={s.location}>{location}</p>
       </div>
 
-      <ul className="stats">
-        <li>
+      <ul className={s.stats}>
+        <li className={s.list}>
           <span className="label">Followers</span>
-          <span className="quantity">{stats.followers}</span>
+          <span className={s.quantity}>{stats.followers}</span>
         </li>
-        <li>
+        <li className={s.list}>
           <span className="label">Views</span>
-          <span className="quantity">{stats.views}</span>
+          <span className={s.quantity}>{stats.views}</span>
         </li>
-        <li>
+        <li className={s.list}>
           <span className="label">Likes</span>
-          <span className="quantity">{stats.likes}</span>
+          <span className={s.quantity}>{stats.likes}</span>
         </li>
       </ul>
     </div>
