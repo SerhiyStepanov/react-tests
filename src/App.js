516eba1f4df7c86e5fbfe445a6e3493d97e8dc06
SerhiyStepanov <SerhiyStepanov@users.noreply.github.com>
2020-11-29 14:17:44 +0200

    task 4

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Profile from "./components/User";
 import Statistics from "./components/Statistics";
 import FriendList from "./components/FriendList";
+import TransactionHistory from "./components/TransactionHistory";
 import user from "./user-data.json";
 import statistics from "./statistical-data.json";
 import friends from "./friends-data.json";
+import transactions from "./transactions-data.json";
 
 export default function App() {
   return (
@@ -19,6 +21,8 @@ export default function App() {
       <Statistics title="Upload stats" stats={statistics} />
 
       <FriendList friends={friends} />
+
+      <TransactionHistory items={transactions} />
     </div>
   );
 }
