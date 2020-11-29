79c19490db81158bbc185783bfb263729bfa95fd
SerhiyStepanov <SerhiyStepanov@users.noreply.github.com>
2020-11-29 21:46:16 +0200

    h-w 01 react

--- src/components/FriendListItem.js
+++ src/components/FriendListItem.js
@@ -2,9 +2,15 @@ import PropTypes from "prop-types";
 import s from "./FriendListItem.module.css";
 
 export default function FriendListItem({ avatar, name, isOnline, id }) {
+  const OnLine = s.activ;
+  const OffLine = s.Off;
   return (
     <li className={s.item}>
-      <span className={s.status}></span>
+      {isOnline ? (
+        <span className={OnLine}></span>
+      ) : (
+        <span className={OffLine}></span>
+      )}
       <img className={s.avatar} src={avatar} alt={name} width="48" />
       <p className={s.name}>{name}</p>
     </li>
